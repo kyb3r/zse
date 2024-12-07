17d52d625519f3bdedc72318f449fc631009114e
KAREEM AGHA <>
2024-12-08 10:21:12 +1100

    fix:linted setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
+"""Setup file for pip installs"""
 from setuptools import setup
 
-with open("requirements.txt") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
